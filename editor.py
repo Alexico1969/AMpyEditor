e6ba8c8b4b1861505514942affe36cbb54ffd636
alexico1969 <your_donotreply_email_id>
2024-06-15 12:02:06 -0400

    first attempt to make the 'input()' statement work

--- editor.py
+++ editor.py
@@ -4,6 +4,7 @@ import io
 import contextlib
 
 editor_blueprint = Blueprint('editor', __name__)
+execution_context = {}
 
 @editor_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
@@ -12,12 +13,48 @@ def editor():
     output = None
     if request.method == 'POST':
         code = request.form['code']
+        execution_context['code'] = code
+        execution_context['output'] = ""
+        execution_context['input_needed'] = False
+        execution_context['exec_globals'] = {
+            'input': get_input,
+            '__name__': '__main__'
+        }
         try:
-            # Capture the output of the code
-            with io.StringIO() as buf, contextlib.redirect_stdout(buf):
-                exec(code)
-                output = buf.getvalue()
+            execute_code(code)
+            output = execution_context['output']
         except Exception as e:
             output = str(e)
 
     return render_template('editor.html', code=code, output=output)
+
+@editor_blueprint.route('/input', methods=['POST'])
+@login_required
+def user_input():
+    user_input = request.form['user_input']
+    execution_context['input_value'] = user_input
+    execution_context['input_needed'] = False
+    try:
+        execute_code(execution_context['remaining_code'])
+        output = execution_context['output']
+    except Exception as e:
+        output = str(e)
+    return jsonify({'output': output})
+
+def get_input(prompt=''):
+    execution_context['output'] += prompt
+    execution_context['input_needed'] = True
+    raise InputNeededException()
+
+def execute_code(code):
+    with io.StringIO() as buf, contextlib.redirect_stdout(buf):
+        try:
+            exec(code, execution_context['exec_globals'])
+        except InputNeededException:
+            execution_context['remaining_code'] = code.splitlines()[1:]
+            execution_context['output'] += buf.getvalue()
+            raise
+        execution_context['output'] += buf.getvalue()
+
+class InputNeededException(Exception):
+    pass
